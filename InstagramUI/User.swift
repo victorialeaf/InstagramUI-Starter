6dba24a11ba57743d867f73ad1351a7ea926d8cf
victorialeaf <32233962+victorialeaf@users.noreply.github.com>
2017-11-08 23:10:13 -0600

    Initial Commit

---
+++ InstagramUI/User.swift
@@ -0,0 +1,38 @@
+//
+//  User.swift
+//  Moments
+//
+//  Created by Duc Tran on 10/9/16.
+//  Copyright © 2016 Developers Academy. All rights reserved.
+//
+
+import UIKit
+
+struct User
+{
+    var username: String?
+    var profileImage: UIImage?
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
